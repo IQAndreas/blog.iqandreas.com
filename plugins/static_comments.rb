fde1d3c728070410483a459be6790a5b589aab2d
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2013-01-15 16:14:05 +0100

    Updated source Tue Jan 15 16:14:05 CET 2013

--- plugins/static_comments.rb
+++ plugins/static_comments.rb
@@ -23,8 +23,8 @@ class Jekyll::Post
 	
 	def to_liquid
 		data = to_liquid_without_comments
-		data['comments'] = StaticComments::find_for_post(self)
-		data['comment_count'] = data['comments'].length
+		data['comment_list'] = StaticComments::find_for_post(self)
+		data['comment_count'] = data['comment_list'].length
 		data
 	end
 end
@@ -32,24 +32,23 @@ end
 module StaticComments
 	# Find all the comments for a post
 	def self.find_for_post(post)
-		@comments ||= read_comments(post.site.source)
-		@comments[post.id]
+		@comment_list ||= read_comments(post.site.source)
+		@comment_list[post.id]
 	end
 	
 	# Read all the comments files in the site, and return them as a hash of
 	# arrays containing the comments, where the key to the array is the value
 	# of the 'post_id' field in the YAML data in the comments files.
 	def self.read_comments(source)
-		comments = Hash.new() { |h, k| h[k] = Array.new }
+		comment_list = Hash.new() { |h, k| h[k] = Array.new }
 		
 		Dir["#{source}/**/_comments/**/*"].sort.each do |comment|
 			next unless File.file?(comment) and File.readable?(comment)
 			yaml_data = YAML::load_file(comment)
 			post_id = yaml_data.delete('post_id')
-			puts "Comment on: " + post_id
-			comments[post_id] << yaml_data
+			comment_list[post_id] << yaml_data
 		end
 		
-		comments
+		comment_list
 	end
 end
