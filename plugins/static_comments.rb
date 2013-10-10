6ba6dabd6ec31ae76744a167f5fcfd3aeb38556f
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2013-10-11 01:57:19 +0200

    Update 'jekyll-static-comments'

--- plugins/static_comments.rb
+++ plugins/static_comments.rb
@@ -1,6 +1,6 @@
 # Store and render comments as a static part of a Jekyll site
 #
-# See README.mdwn for detailed documentation on this plugin.
+# See README.md for detailed documentation on this plugin.
 #
 # Homepage: http://theshed.hezmatt.org/jekyll-static-comments
 #
@@ -21,34 +21,166 @@
 class Jekyll::Post
 	alias :to_liquid_without_comments :to_liquid
 	
-	def to_liquid
-		data = to_liquid_without_comments
-		data['comment_list'] = StaticComments::find_for_post(self)
+	def to_liquid(attrs = nil)
+		data = to_liquid_without_comments(attrs)
+		data['comment_list'] = StaticComments::find_for(self.site, data['id'])
+		data['comment_count'] = data['comment_list'].length
+		data
+	end
+end
+
+class Jekyll::Page
+	alias :to_liquid_without_comments :to_liquid
+	
+	def to_liquid(attrs = nil)
+		data = to_liquid_without_comments(attrs)
+		data['comment_list'] = StaticComments::find_for(self.site, data['id'])
 		data['comment_count'] = data['comment_list'].length
 		data
 	end
 end
 
 module StaticComments
-	# Find all the comments for a post
-	def self.find_for_post(post)
-		@comment_list ||= read_comments(post.site.source)
-		@comment_list[post.id]
+	
+	# Find all the comments for a post or page with the specified id
+	def self.find_for(site, id)
+		@comment_list ||= read_comments(site)
+		@comment_list[id]
 	end
 	
 	# Read all the comments files in the site, and return them as a hash of
 	# arrays containing the comments, where the key to the array is the value
-	# of the 'post_id' field in the YAML data in the comments files.
-	def self.read_comments(source)
+	# of the 'page_id' field in the YAML data in the comments files.
+	def self.read_comments(site)
 		comment_list = Hash.new() { |h, k| h[k] = Array.new }
 		
-		Dir["#{source}/**/_comments/**/*"].sort.each do |comment|
-			next unless File.file?(comment) and File.readable?(comment)
-			yaml_data = YAML::load_file(comment)
-			post_id = yaml_data.delete('post_id')
-			comment_list[post_id] << yaml_data
+		source=site.source
+		Dir["#{source}/**/_comments/**/*"].sort.each do |comment_filename|
+			next unless File.file?(comment_filename) and File.readable?(comment_filename)
+			# `comment_filename` includes everything: the full, absolute path, file name, and extension.
+			comment = Comment.new(site, comment_filename)
+			if (comment.published?)
+				comment_list[comment.page_id] << comment
+			end
 		end
 		
 		comment_list
 	end
+	
+	class Comment 
+		include Jekyll::Convertible
+		
+		# Attributes for Liquid templates
+		ATTRIBUTES_FOR_LIQUID = %w[
+			page_id
+			date
+			name
+			link
+			content
+			published
+		]
+		
+		attr_accessor :site
+		attr_accessor :data, :content
+		attr_accessor :page_id, :date, :published
+		attr_accessor :name, :link
+		attr_accessor :path, :dir, :name, :basename, :ext, :output
+		
+		def initialize(site, full_filename)
+			@site = site
+			self.process(full_filename)
+			
+			self.read_yaml(@dir, @name)
+			@date      = self.data['date']
+			@name      = self.data['name']
+			@link      = self.data['link']
+			@published = self.published?
+			
+			# Reverse compatiblitiy with previous versions of `jekyll-static-comments` wich called the "page_id" field "post_id"
+			if (data.key?('post_id'))
+				@page_id = self.data['post_id']
+			else
+				@page_id = self.data['page_id']
+			end
+			
+			# Reverse compatiblitiy with previous versions of `jekyll-static-comments` wich called the "content" field "comment"
+			if (data.key?('comment'))
+				@content = data['comment']
+				data.delete('comment')
+			end
+			
+			
+			# First the script goes through a few custom converters (I don't want to interfere with the rest
+			# of Jekyll just in case). If none is profided, just use one of the builtin converters.
+			# The default converter that handles the type of extension can be found and adjusted in `configuration.rb`
+			@converter = custom_converter || converter
+			
+			self.transform()
+		end
+		
+		def process(full_filename)
+			@path =     full_filename # Required by Jekyll::Convertible
+			@name =     File.basename(full_filename)
+			@ext =      File.extname(full_filename)
+			@dir =      File.dirname(full_filename)
+			@basename = File.basename(@name, @ext)
+		end
+		
+		def custom_converter
+			if (PlaintextConverter::matches(self.ext))
+				PlaintextConverter.new()
+			elsif (HTMLConverter::matches(self.ext))
+				HTMLConverter.new(true, true)
+			else
+				nil
+			end
+		end
+		
+		def published?
+			if self.data.has_key?('published') && self.data['published'] == false
+				false
+			else
+				true
+			end
+		end
+		
+	end
+	
+	class PlaintextConverter
+		include Liquid::StandardFilters
+		def self.matches(ext)
+			ext.eql?('.txt')
+		end
+	
+		def convert(content)
+			content = escape(content)
+			content = newline_to_br(content)
+			content
+		end
+	end
+	
+	class HTMLConverter
+		include Liquid::StandardFilters
+		def self.matches(ext)
+			ext.eql?('.html') or ext.eql?('.htm')
+		end
+		
+		def initialize(basic_html, newlines_to_br)
+			@basic_html = basic_html 		 # Strip out some potentially "unwanted" elements such as <script>
+			@newlines_to_br = newlines_to_br # Convert all found newlines into <br /> tags
+		end
+		
+		def convert(content)
+			if (@basic_html)
+				content = content.gsub(/<script.*?<\/script>/m, '')
+				content = content.gsub(/<!--.*?-->/m, '')
+				content = content.gsub(/<style.*?<\/style>/m, '')
+			end
+			if (@newlines_to_br)
+				content = newline_to_br(content)
+			end
+			content
+		end
+	end
 end
+
---
+++ source/_posts/_comments/2012-03-08-the-problems-with-being-self-taught/-monologues-the-problems-with-being-self-taught-comment-2013-10-05-22-52-51.md
@@ -0,0 +1,16 @@
+---
+post_id: /monologues/the-problems-with-being-self-taught
+date: 2013-10-05 22:52
+post_title: 'The Problems with Being Self Taught'
+return_url: 'http://blog.iqandreas.com/monologues/the-problems-with-being-self-taught/'
+name: 'Mike'
+email: 'michael.t.rich@gmail.com'
+link: ''
+---
+I'm a self taught programmer too, started at the age of sixteen now I'm 24.
+
+The problem with being your own teacher is that, if you're not a good teacher then you're not going to learn very well. A great example of this is the wanna-be hacker who goes out to the library to find books, only to read them without checking the publish date. Not knowing that he/she is learning is 2 years out of date. Yes I was one of those, but replace hacking with learning HTML.
+
+Good teachers will not only teach you the fundamentals of a programming language, they'll also gear you with current technologies that are likely to land you with a job in the future. Again, something self-taughts can't do.
+
+What I wish I did differently is go to University, and to also self-learn.
