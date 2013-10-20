26d1944b1da2988b7271ff82735970cd3102cac5
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2013-10-20 05:34:37 +0200

    Fix error in MineCraft mod titles

--- source/_minecraft/move_minecraft_filter.rb
+++ source/_minecraft/move_minecraft_filter.rb
@@ -17,9 +17,9 @@ class MoveMinecraftFilter < Jekyll::PostFilter
 			data =  "---\n"
 			data += "permalink: /minecraft/#{name}.html\n"
 			data += "layout: redirect-minecraft\n"
-			data += "redirect: #{new_url}\n"
-			data += "redirect_message: This mod can now be found on my MineCraft site (redirecting there automatically): <li class=\"flink minecraft.iqandreas.com\"><a href=\"#{new_url}\">#{title}</a></li>\n"
-			data += "title: #{title}\n"
+			data += "redirect: '#{new_url}'\n"
+			data += "redirect_message: 'This mod can now be found on my MineCraft site (redirecting there automatically): <li class=\"flink minecraft.iqandreas.com\"><a href=\"#{new_url}\">#{title}</a></li>'\n"
+			data += "title: '#{title}'\n"
 			data += "name: #{name}\n"
 			data +=  "---\n"
 			data +=  page_or_post.to_s()
